de3cf5cc1ae108ec815ba6a317a53e5673c666be
Ziaja, Łukasz <lukasz.ziaja@atos.net>
2017-12-30 17:09:03 +0100

    TODO: displaying user in the front

---
+++ src/main/java/com/rfb/service/dto/RfbEventAttendanceDTO.java
@@ -0,0 +1,85 @@
+package com.rfb.service.dto;
+
+
+import com.rfb.domain.RfbEvent;
+
+import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+/**
+ * A DTO for the RfbEventAttendance entity.
+ */
+public class RfbEventAttendanceDTO implements Serializable {
+
+    private Long id;
+
+    private LocalDate attendanceDate;
+
+    private RfbEventDTO rfbEventDTO;
+
+    private RfbUserDTO rfbUserDTO;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getAttendanceDate() {
+        return attendanceDate;
+    }
+
+    public void setAttendanceDate(LocalDate attendanceDate) {
+        this.attendanceDate = attendanceDate;
+    }
+
+    public RfbEventDTO getRfbEventDTO() {
+        return rfbEventDTO;
+    }
+
+    public void setRfbEventDTO(RfbEventDTO rfbEventDTO) {
+        this.rfbEventDTO = rfbEventDTO;
+    }
+
+    public RfbUserDTO getRfbUserDTO() {
+        return rfbUserDTO;
+    }
+
+    public void setRfbUserDTO(RfbUserDTO rfbUserDTO) {
+        this.rfbUserDTO = rfbUserDTO;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RfbEventAttendanceDTO rfbEventAttendanceDTO = (RfbEventAttendanceDTO) o;
+        if(rfbEventAttendanceDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), rfbEventAttendanceDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "RfbEventAttendanceDTO{" +
+            "id=" + getId() +
+            ", attendanceDate='" + getAttendanceDate() + "'" +
+            "}";
+    }
+}
